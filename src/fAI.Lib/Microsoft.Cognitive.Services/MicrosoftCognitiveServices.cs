22a888ca47ecc64d98e202caeec6d427f7a67f8f
Frederic Torres <frederic.torres@bigtincan.com>
2023-11-10 14:04:05 -0500

    Added Microsoft.CognitiveServices.Speech

---
+++ src/fAI.Lib/Microsoft.Cognitive.Services/MicrosoftCognitiveServices.cs
@@ -0,0 +1,102 @@
+﻿using Microsoft.CognitiveServices.Speech;
+using Microsoft.CognitiveServices.Speech.Audio;
+using System;
+
+namespace fAI
+{
+    internal class TextToSpeechProviderException : Exception
+    {
+        public TextToSpeechProviderException(string message)
+            : base(message)
+        {
+        }
+        public TextToSpeechProviderException(string message, Exception innerException)
+            : base(message, innerException)
+        {
+        }
+    }
+
+    public enum AudioType
+    {
+        WAV,
+        MP3
+    }
+
+    public class MicrosoftCognitiveServices
+    {
+        SpeechConfig _config;
+        
+        string _key     = Environment.GetEnvironmentVariable("MICROSOFT_COGNITIVE_SERVICES_KEY");
+        string _region  = Environment.GetEnvironmentVariable("MICROSOFT_COGNITIVE_SERVICES_REGION");
+
+        public MicrosoftCognitiveServices(string key = null, string region = "eastus")
+        {
+            if(_key != null)
+                _key = key;
+
+            if(_region != null)
+                _region = region;
+
+            _config = SpeechConfig.FromSubscription(_key, _region);
+        }
+
+        private AudioConfig GetAudioConfiguration(string fileName, AudioType audioType)
+        {
+            var audioConfig = AudioConfig.FromWavFileOutput(fileName);
+            if (audioType == AudioType.MP3)
+                _config.SetSpeechSynthesisOutputFormat(SpeechSynthesisOutputFormat.Audio48Khz96KBitRateMonoMp3); 
+            else
+                _config.SetSpeechSynthesisOutputFormat(SpeechSynthesisOutputFormat.Riff44100Hz16BitMonoPcm);
+            return audioConfig;
+        }
+
+        public  void CreateAudioFile(string text, string voiceName, string fileName)
+        {
+            var ssml = GetSSML(text, voiceName);
+            __createAudioFileSSML(ssml, voiceName, fileName);
+        }
+
+        private static string GetSSML(string text, string voiceName)
+        {
+            return $@"<speak xmlns=""http://www.w3.org/2001/10/synthesis"" xmlns:mstts=""http://www.w3.org/2001/mstts"" xmlns:emo=""http://www.w3.org/2009/10/emotionml"" version=""1.0"" xml:lang=""en-US"">
+                            <voice name=""{voiceName}"">{text}</voice>
+                    </speak>";
+        }
+
+        private void __createAudioFileSSML(string ssml, string voiceName, string fileName)
+        {
+            var audioType = AudioType.MP3;
+
+            _config.SpeechSynthesisLanguage = "en-us";
+            _config.SpeechSynthesisVoiceName = voiceName;
+
+            this.RemoveFile(fileName);
+
+            var audioConfig = GetAudioConfiguration(fileName, audioType);
+
+            using (var synthetizer = new SpeechSynthesizer(_config, audioConfig))
+            {
+                SpeechSynthesisResult r = synthetizer.SpeakTextAsync(ssml).GetAwaiter().GetResult();
+                if (r.Reason != ResultReason.SynthesizingAudioCompleted)
+                    throw new TextToSpeechProviderException($"Error generating audio from text: {r.Reason}");
+            }
+        }
+
+        public bool RemoveFile(string fileName)
+        {
+            if (!System.IO.File.Exists(fileName))
+                return false;
+
+            try
+            {
+                System.IO.File.Delete(fileName);
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+    }
+}
+
---
+++ src/fAI.Tests/Microsoft.Cognitive.Services/Microsoft.Cognitive.Services.Tests.cs
@@ -0,0 +1,31 @@
+﻿using System.IO;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+using System.Linq;
+using System;
+using fAI;
+using Xunit;
+using static fAI.GPT;
+
+namespace fAI.Tests
+{
+    [Collection("Sequential")]
+    [CollectionDefinition("Sequential", DisableParallelization = true)]
+    public class MicrosoftCognitiveServicesTests
+    {
+        [Fact()]
+        public void GetModels()
+        {
+            var text = @"I am he as you are he as you are me
+And we are all together
+See how they run like pigs from a gun
+See how they fly
+I'm crying";
+            var mcs = new MicrosoftCognitiveServices();
+            var voiceId = "en-US-DavisNeural";
+            var mp3FileName = Path.Combine(Path.GetTempPath(), "mp3.mp3");
+            mcs.CreateAudioFile(text, voiceId, mp3FileName);
+          
+        }
+    }
+}
