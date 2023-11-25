18a37ab01b3f8794573a2554af5459681d705af3
Frederic Torres <frederic.torres@bigtincan.com>
2023-11-24 19:01:42 -0500

    Image DALL-E

--- src/fAI.Lib/GeneratedDocument/GeneratedDocuments.cs
+++ src/fAI.Lib/GeneratedDocument/GeneratedDocuments.cs
@@ -21,7 +21,7 @@ namespace fAI
     {
         public string Title { get; set; }
         public string Description { get; set; }
-
+        public DateTime CreatedDate { get; set; } = DateTime.Now;
     }
 
 
--- src/fAIConsole/Program.cs
+++ src/fAIConsole/Program.cs
@@ -33,45 +33,31 @@ I'm crying";
         // The novel explores themes of love, and societal rejection.
         // However, Esmeralda is also desired by Archdeacon Frollo, Quasimodo's adoptive father, and Captain Phoebus.
         // He falls in love with a beautiful gypsy dancer, Esmeralda, who shows him kindness.
-        const string TheHunchbackOfNotreDame_Summary_Sanitized = @"As French literature expert and people expert.
-Generate an image inspired by Victor Hugo's classic novel, 'The Hunchback of Notre-Dame'.
-Also used the following book's summary as a prompt:
+        const string TheHunchbackOfNotreDame_Summary_Sanitized = @"
 ""The Hunchback of Notre-Dame"" is a tragic tale set in 15th century Paris. 
 The story revolves around Quasimodo, a bell-ringer of Notre-Dame Cathedral. 
 He falls in love with a beautiful dancer, Esmeralda, who shows him kindness.
 However, Esmeralda is also desired by Archdeacon Frollo, Quasimodo's adoptive father, and Captain Phoebus.
-
-The image should depict 3 characters from France only and from the era of the novel.
-The image should be a painting, not a photograph.
-
-About the image to be generated:
-- Level of Detail: High.
 ";
 
-        const string BugJargal_Summary_Sanitized = @"As French literature expert and people expert.
-Generate an image inspired by Victor Hugo's classic novel, 'Bug-Jargal'.
-Also used the following book's summary as a prompt:
-""Bug-Jargal"" is a historical novel by Victor Hugo set during the slave revolt in Santo Domingo in 1791. 
+        const string BugJargal_Summary_Sanitized = @"
+\""Bug-Jargal"" is a historical novel by Victor Hugo set during the revolt in Santo Domingo in 1791. 
 The protagonist, a French military officer named Leopold d'Auverney, recounts his experiences during the revolt. 
-He befriends Bug-Jargal, a noble African slave who becomes a revolutionary leader. Despite their differences, they form a deep bond. 
+He befriends Bug-Jargal, a noble African who becomes a revolutionary leader.
+Despite their differences, they form a deep bond.
 The novel explores themes of inequality, friendship, and the struggle for freedom. 
 It also highlights the brutality of slavery and the courage of those who fought against it. 
-The story ends tragically with Bug-Jargal's execution, symbolizing the high cost of freedom.
-
-The image should depict 3 characters from France only and from the era of the novel.
-The image should be a painting, not a photograph.
-
-About the image to be generated:
-- Level of Detail: High.""";
+";
 
         // Victor Hugo's MasterPieces
         public static Dictionary<string, string> Books = new Dictionary<string, string>()
         {
+            ["Bug-Jargal"] = BugJargal_Summary_Sanitized,
             ["Les Misérables"                   ] = "",
             ["The Hunchback of Notre-Dame"      ] = TheHunchbackOfNotreDame_Summary_Sanitized,
             ["Ninety-Three"                     ] = "",
             ["The Man Who Laughs"               ] = "",
-            ["Bug-Jargal"                       ] = BugJargal_Summary_Sanitized,
+            //["Bug-Jargal"                       ] = BugJargal_Summary_Sanitized,
             ["The Toilers of the Sea"           ] = "",
             ["The Last Day of a Condemned Man"  ] = "",
             ["Hans of Iceland"                  ] = "",
@@ -82,9 +68,8 @@ About the image to be generated:
         public static void Generate_Document()
         {
             var generatedDocuments = new GeneratedDocuments();
-            generatedDocuments.Properties.Title = "Victor Hugo's MasterPieces";
-            generatedDocuments.Properties.Description = "Generate a summary of Victor Hugo's MasterPieces and an image inspired by the book.";
-            
+            generatedDocuments.Properties.Title         = "Victor Hugo's MasterPieces";
+            generatedDocuments.Properties.Description   = "Generate a summary of Victor Hugo's MasterPieces and an image inspired by the book.";
             OpenAI.TraceOn = true;
             OpenAI.Trace($"Generating document about {generatedDocuments.Properties.Title}", new { });
 
@@ -92,7 +77,6 @@ About the image to be generated:
             foreach (var book in Books.Keys)
             {
                 var generatedDocument = generatedDocuments.Add(book);
-
                 var prompt = new Prompt_GPT_4
                 {
                     Messages = new List<GPTMessage> {
@@ -112,17 +96,19 @@ About the image to be generated:
                     var imagePrompt = $@"
 As French literature expert and people expert.
 Generate an image inspired by Victor Hugo's classic novel, '{book}'.
+Also used the following book's summary as a prompt:
 [MORE_DALLE_PROMPT]
 
 The image should depict 3 characters from France only and from the era of the novel.
 The image should be a painting, not a photograph.
 
 About the image to be generated:
+- Level of Detail: High.
 /*
 - Artistic Style: Impressionism.
 - Color Scheme: Vincent Van Gogh color palette.
 */
-- Level of Detail: High.".RemoveComment(StringComment.C);
+".RemoveComment(StringComment.C);
 
                     var useSanitizedSummaryVersion = !string.IsNullOrEmpty(Books[book]); // Avoid our content policy from openAI, about image generation.
                     if (useSanitizedSummaryVersion)
