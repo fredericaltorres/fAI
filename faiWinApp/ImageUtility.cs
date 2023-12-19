5176f387b3a0b22720e7484c12fad31259315a0b
Frederic Torres <frederic.torres@bigtincan.com>
2023-12-19 01:03:58 -0500

    Add fAi Win Utility App

--- faiWinApp/ImageUtility.cs
+++ faiWinApp/ImageUtility.cs
@@ -15,10 +15,56 @@ namespace faiWinApp
         public static List<string> SliceImageBy4(string originalImage)
         {
             var r = new List<string>();
+            r.Add(SliceImageBy4_TopRight(originalImage));
+            r.Add(SliceImageBy4_BottomRight(originalImage));
             r.Add(SliceImageBy4_TopLeft(originalImage));
             r.Add(SliceImageBy4_BottomLeft(originalImage));
             return r;
         }
+
+        public static string SliceImageBy4_BottomRight(string filePath)
+        {
+            string originalImagePath = filePath;
+            string newImagePath = GetNewImageFileName(ImageFormat.Png);
+
+            using (Bitmap originalImage = new Bitmap(originalImagePath))
+            {
+                int width = originalImage.Width / 2;
+                int height = originalImage.Height / 2; // Quarter height
+                int startY = originalImage.Height - height; // Starting Y-coordinate for the bottom quarter
+                int startX = originalImage.Width - width;
+
+                Rectangle cropArea = new Rectangle(startX, startY, width, height);
+
+                using (Bitmap croppedImage = originalImage.Clone(cropArea, originalImage.PixelFormat))
+                {
+                    croppedImage.Save(newImagePath); // Save the bottom quarter of the image
+                }
+                return newImagePath;
+            }
+        }
+
+        public static string SliceImageBy4_TopRight(string filePath)
+        {
+            string originalImagePath = filePath;
+            string newImagePath = GetNewImageFileName(ImageFormat.Png);
+
+            using (Bitmap originalImage = new Bitmap(originalImagePath))
+            {
+                int width = originalImage.Width / 2;
+                int height = originalImage.Height / 2;
+                int startX = originalImage.Width - width;
+
+                var cropArea = new Rectangle(startX, 0, width, height);
+
+                using (Bitmap croppedImage = originalImage.Clone(cropArea, originalImage.PixelFormat))
+                {
+                    croppedImage.Save(newImagePath); // Save the top quarter of the image
+                }
+                return newImagePath;
+            }
+        }
+
         public static string SliceImageBy4_TopLeft(string filePath)
         {
             string originalImagePath = filePath;
