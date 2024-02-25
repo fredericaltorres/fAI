a2a22daa33403b841d455f3fd2ee9613976c68cb
frederic.torres@bigtincan.com <frederic.torres@bigtincan.com>
2024-02-25 13:05:19 -0500

    Update...

--- src/fAI.Tests/OpenAI/OpenAi.Complettions.Chat.cs
+++ src/fAI.Tests/OpenAI/OpenAi.Complettions.Chat.cs
@@ -119,8 +119,7 @@ namespace fAI.Tests
             };
             var response = client.Completions.Create(prompt);
             Assert.True(response.Success);
-            DS.Assert.Words(response.Text, "error & message & mismatch Folder & Description & AssertDetails & regular & expression");
-            
+            DS.Assert.Words(response.Text, "error & message & mismatch & Order & expecting & Description & Camembert & Gorgonzola & Toillete");
 
             var blogPost = response.BlogPost;
             Assert.Contains("Model:", blogPost);
@@ -265,124 +264,124 @@ End of text
         }
 
    
-        [Fact()]
-        [TestBeforeAfter]
-        public void Translate_EnglishToFrench_SpecialRules()
-        {
-            var client = new OpenAI();
-            var text = "10%";
-            Assert.False(client.Completions.NeedToBeTranslated(text, TranslationLanguages.English, TranslationLanguages.French), $"NeedToBeTranslated text:{text}");
+        //[Fact()]
+        //[TestBeforeAfter]
+        //public void Translate_EnglishToFrench_SpecialRules()
+        //{
+        //    var client = new OpenAI();
+        //    var text = "10%";
+        //    Assert.False(client.Completions.NeedToBeTranslated(text, TranslationLanguages.English, TranslationLanguages.French), $"NeedToBeTranslated text:{text}");
 
-            text = "10% cheaper";
-            Assert.True(client.Completions.NeedToBeTranslated(text, TranslationLanguages.English, TranslationLanguages.French), $"NeedToBeTranslated text:{text}");
+        //    text = "10% cheaper";
+        //    Assert.True(client.Completions.NeedToBeTranslated(text, TranslationLanguages.English, TranslationLanguages.French), $"NeedToBeTranslated text:{text}");
 
-            text = "200K";
-            Assert.False(client.Completions.NeedToBeTranslated(text, TranslationLanguages.English, TranslationLanguages.French), $"NeedToBeTranslated text:{text}");
+        //    text = "200K";
+        //    Assert.False(client.Completions.NeedToBeTranslated(text, TranslationLanguages.English, TranslationLanguages.French), $"NeedToBeTranslated text:{text}");
 
-            text = "200K cheaper";
-            Assert.True(client.Completions.NeedToBeTranslated(text, TranslationLanguages.English, TranslationLanguages.French), $"NeedToBeTranslated text:{text}");
+        //    text = "200K cheaper";
+        //    Assert.True(client.Completions.NeedToBeTranslated(text, TranslationLanguages.English, TranslationLanguages.French), $"NeedToBeTranslated text:{text}");
 
-            text = "$200K";
-            Assert.True (client.Completions.NeedToBeTranslated(text, TranslationLanguages.English, TranslationLanguages.French), $"NeedToBeTranslated text:{text}");
+        //    text = "$200K";
+        //    Assert.True (client.Completions.NeedToBeTranslated(text, TranslationLanguages.English, TranslationLanguages.French), $"NeedToBeTranslated text:{text}");
 
-            text = "3,948";
-            Assert.True(client.Completions.NeedToBeTranslated(text, TranslationLanguages.English, TranslationLanguages.French), $"NeedToBeTranslated text:{text}");
+        //    text = "3,948";
+        //    Assert.True(client.Completions.NeedToBeTranslated(text, TranslationLanguages.English, TranslationLanguages.French), $"NeedToBeTranslated text:{text}");
 
-            text = "3,948,123";
-            Assert.True(client.Completions.NeedToBeTranslated(text, TranslationLanguages.English, TranslationLanguages.French), $"NeedToBeTranslated text:{text}");
-        }
+        //    text = "3,948,123";
+        //    Assert.True(client.Completions.NeedToBeTranslated(text, TranslationLanguages.English, TranslationLanguages.French), $"NeedToBeTranslated text:{text}");
+        //}
 
-        [Fact()]
-        [TestBeforeAfter]
-        public void Translate_EnglishToFrench_SpecialCase_NumberWithCommaAsThousandSeparator()
-        {
-            var client = new OpenAI();
-            var text = "3,948";
-            var translation = client.Completions.Translate(text, TranslationLanguages.English, TranslationLanguages.French);
-            Assert.True(FlexStrCompare("3 948") == FlexStrCompare(translation));
-        }
+        //[Fact()]
+        //[TestBeforeAfter]
+        //public void Translate_EnglishToFrench_SpecialCase_NumberWithCommaAsThousandSeparator()
+        //{
+        //    var client = new OpenAI();
+        //    var text = "3,948";
+        //    var translation = client.Completions.Translate(text, TranslationLanguages.English, TranslationLanguages.French);
+        //    Assert.True(FlexStrCompare("3 948") == FlexStrCompare(translation));
+        //}
 
-        [Fact()]
-        [TestBeforeAfter]
-        public void Translate_EnglishToFrench_SpecialCase_Dollars200K()
-        {
-            var client = new OpenAI();
-            var text = "$200K";
-            var translation = client.Completions.Translate(text, TranslationLanguages.English, TranslationLanguages.French);
-            Assert.True(FlexStrCompare("2 000 $") == FlexStrCompare(translation));
-        }
+        //[Fact()]
+        //[TestBeforeAfter]
+        //public void Translate_EnglishToFrench_SpecialCase_Dollars200K()
+        //{
+        //    var client = new OpenAI();
+        //    var text = "$200K";
+        //    var translation = client.Completions.Translate(text, TranslationLanguages.English, TranslationLanguages.French);
+        //    Assert.True(FlexStrCompare("2 000 $") == FlexStrCompare(translation));
+        //}
 
-        [Fact()]
-        [TestBeforeAfter]
-        public void Translate_EnglishToFrench_SpecialCase_200K()
-        {
-            var client = new OpenAI();
-            var text = "200K";
-            var translation = client.Completions.Translate(text, TranslationLanguages.English, TranslationLanguages.French);
-            Assert.True(FlexStrCompare(text) == FlexStrCompare(translation));
-        }
+        //[Fact()]
+        //[TestBeforeAfter]
+        //public void Translate_EnglishToFrench_SpecialCase_200K()
+        //{
+        //    var client = new OpenAI();
+        //    var text = "200K";
+        //    var translation = client.Completions.Translate(text, TranslationLanguages.English, TranslationLanguages.French);
+        //    Assert.True(FlexStrCompare(text) == FlexStrCompare(translation));
+        //}
 
-        [Fact()]
-        [TestBeforeAfter]
-        public void Translate_EnglishToFrench()
-        {
-            var client = new OpenAI();
-            var translation = client.Completions.Translate(ReferenceEnglishSentence, TranslationLanguages.English, TranslationLanguages.French);
-            Assert.True(FlexStrCompare("Bonjour monde.") == FlexStrCompare(translation) ||
-                        FlexStrCompare("Bonjour le monde.") == FlexStrCompare(translation));
-        }
+        //[Fact()]
+        //[TestBeforeAfter]
+        //public void Translate_EnglishToFrench()
+        //{
+        //    var client = new OpenAI();
+        //    var translation = client.Completions.Translate(ReferenceEnglishSentence, TranslationLanguages.English, TranslationLanguages.French);
+        //    Assert.True(FlexStrCompare("Bonjour monde.") == FlexStrCompare(translation) ||
+        //                FlexStrCompare("Bonjour le monde.") == FlexStrCompare(translation));
+        //}
 
-        [Fact()]
-        [TestBeforeAfter]
-        public void Translate_EnglishToFrench_Dictionary()
-        {
-            var client = new OpenAI();
-            var inputDictionary = JsonConvert.DeserializeObject<Dictionary<string, string>>(ReferenceEnglishJsonDictionary);
-            var outputDictionary = client.Completions.Translate(inputDictionary, TranslationLanguages.English, TranslationLanguages.French);
-            Assert.Equal(6, outputDictionary.Keys.Count);
+        //[Fact()]
+        //[TestBeforeAfter]
+        //public void Translate_EnglishToFrench_Dictionary()
+        //{
+        //    var client = new OpenAI();
+        //    var inputDictionary = JsonConvert.DeserializeObject<Dictionary<string, string>>(ReferenceEnglishJsonDictionary);
+        //    var outputDictionary = client.Completions.Translate(inputDictionary, TranslationLanguages.English, TranslationLanguages.French);
+        //    Assert.Equal(6, outputDictionary.Keys.Count);
 
-            inputDictionary.Keys.ToList().ForEach(k => Assert.True(outputDictionary.ContainsKey(k)));
+        //    inputDictionary.Keys.ToList().ForEach(k => Assert.True(outputDictionary.ContainsKey(k)));
 
-            Assert.Equal("Éducation", outputDictionary["(50,52)"]);
-            Assert.Equal("Salle de classe 01", outputDictionary["(53,54)"]);
-        }
+        //    Assert.Equal("Éducation", outputDictionary["(50,52)"]);
+        //    Assert.Equal("Salle de classe 01", outputDictionary["(53,54)"]);
+        //}
 
         
-        [Fact()]
-        [TestBeforeAfter]
-        public void Translate_EnglishToFrench_List()
-        {
-            var client = new OpenAI();
-            var inputDictionary = JsonConvert.DeserializeObject<Dictionary<string, string>>(ReferenceEnglishJsonDictionary);
-            var inputList = inputDictionary.Values.ToList();
+        //[Fact()]
+        //[TestBeforeAfter]
+        //public void Translate_EnglishToFrench_List()
+        //{
+        //    var client = new OpenAI();
+        //    var inputDictionary = JsonConvert.DeserializeObject<Dictionary<string, string>>(ReferenceEnglishJsonDictionary);
+        //    var inputList = inputDictionary.Values.ToList();
 
-            var outputList = client.Completions.Translate(inputList, TranslationLanguages.English, TranslationLanguages.French);
-            Assert.Equal(6, outputList.Count);
+        //    var outputList = client.Completions.Translate(inputList, TranslationLanguages.English, TranslationLanguages.French);
+        //    Assert.Equal(6, outputList.Count);
 
-            AssertWords(outputList[0], "personnes,important,domaine,activité");
-            //AssertWords(outputList[4], "Graphiques,entreprise");
-        }
+        //    AssertWords(outputList[0], "personnes,important,domaine,activité");
+        //    //AssertWords(outputList[4], "Graphiques,entreprise");
+        //}
 
-        [Fact()]
-        [TestBeforeAfter]
-        public void Translate_EnglishToFrench_ListOfNumber()
-        {
-            var client = new OpenAI();
-            var inputList = DS.List(1, 2, 3, 4).Select(i => i.ToString()).ToList();
+        //[Fact()]
+        //[TestBeforeAfter]
+        //public void Translate_EnglishToFrench_ListOfNumber()
+        //{
+        //    var client = new OpenAI();
+        //    var inputList = DS.List(1, 2, 3, 4).Select(i => i.ToString()).ToList();
 
-            var outputList = client.Completions.Translate(inputList, TranslationLanguages.English, TranslationLanguages.French);
-            Assert.Equal(4, outputList.Count);
-            DS.Range(4).ForEach(i => Assert.Contains((i+1).ToString(), outputList[i]));
-        }
+        //    var outputList = client.Completions.Translate(inputList, TranslationLanguages.English, TranslationLanguages.French);
+        //    Assert.Equal(4, outputList.Count);
+        //    DS.Range(4).ForEach(i => Assert.Contains((i+1).ToString(), outputList[i]));
+        //}
 
-        [Fact()]
-        [TestBeforeAfter]
-        public void Translate_EnglishToSpanish()
-        {
-            var client = new OpenAI();
-            var translation = client.Completions.Translate(ReferenceEnglishSentence, TranslationLanguages.English, TranslationLanguages.Spanish);
-            Assert.Equal("'Hola mundo.'", translation);
-        }
+        //[Fact()]
+        //[TestBeforeAfter]
+        //public void Translate_EnglishToSpanish()
+        //{
+        //    var client = new OpenAI();
+        //    var translation = client.Completions.Translate(ReferenceEnglishSentence, TranslationLanguages.English, TranslationLanguages.Spanish);
+        //    Assert.Equal("'Hola mundo.'", translation);
+        //}
 
         const string ReferenceEnglishTextForSummarization = @"Hey there, everyone! I'm Jordan Lee, and I'm super excited to be here with you today because 
 I've got somethin to share with you that is going to blow your mind!
