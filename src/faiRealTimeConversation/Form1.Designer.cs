057a89f27d4286e52d0ede46b8eca0c20e52aa99
FredericALTorres <frederic.torres@bigtincan.com>
2024-03-27 00:14:48 -0400

    Update...

---
+++ src/faiRealTimeConversation/Form1.Designer.cs
@@ -0,0 +1,118 @@
+﻿namespace faiRealTimeConversation
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.quitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.butTalk = new System.Windows.Forms.Button();
+            this.txtUserOutput = new System.Windows.Forms.TextBox();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Padding = new System.Windows.Forms.Padding(9, 3, 0, 3);
+            this.menuStrip1.Size = new System.Drawing.Size(1053, 30);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.quitToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(44, 24);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // quitToolStripMenuItem
+            // 
+            this.quitToolStripMenuItem.Name = "quitToolStripMenuItem";
+            this.quitToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.Q)));
+            this.quitToolStripMenuItem.Size = new System.Drawing.Size(180, 24);
+            this.quitToolStripMenuItem.Text = "Quit";
+            this.quitToolStripMenuItem.Click += new System.EventHandler(this.quitToolStripMenuItem_Click);
+            // 
+            // butTalk
+            // 
+            this.butTalk.Location = new System.Drawing.Point(465, 436);
+            this.butTalk.Name = "butTalk";
+            this.butTalk.Size = new System.Drawing.Size(131, 78);
+            this.butTalk.TabIndex = 1;
+            this.butTalk.Text = "Talk";
+            this.butTalk.UseVisualStyleBackColor = true;
+            this.butTalk.Click += new System.EventHandler(this.butTalk_Click);
+            // 
+            // txtUserOutput
+            // 
+            this.txtUserOutput.Font = new System.Drawing.Font("Consolas", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtUserOutput.Location = new System.Drawing.Point(24, 45);
+            this.txtUserOutput.Margin = new System.Windows.Forms.Padding(4);
+            this.txtUserOutput.Multiline = true;
+            this.txtUserOutput.Name = "txtUserOutput";
+            this.txtUserOutput.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtUserOutput.Size = new System.Drawing.Size(985, 347);
+            this.txtUserOutput.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 19F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1053, 537);
+            this.Controls.Add(this.txtUserOutput);
+            this.Controls.Add(this.butTalk);
+            this.Controls.Add(this.menuStrip1);
+            this.Font = new System.Drawing.Font("Consolas", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.MainMenuStrip = this.menuStrip1;
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "fAI Real Time Conversation";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem quitToolStripMenuItem;
+        private System.Windows.Forms.Button butTalk;
+        private System.Windows.Forms.TextBox txtUserOutput;
+    }
+}
+
---
+++ src/faiRealTimeConversation/Form1.cs
@@ -0,0 +1,104 @@
+﻿using NAudio.CoreAudioApi;
+using NAudio.Wave;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace faiRealTimeConversation
+{
+    // https://markheath.net/post/30-days-naudio-docs
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void quitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+             this.UserMessage("Welcome to the fAI Real Time Conversation App");
+            MMDeviceEnumerator enumerator = new MMDeviceEnumerator();
+            var outputDevices = enumerator.EnumerateAudioEndPoints(DataFlow.Render, DeviceState.Active);
+            var inputDevices = enumerator.EnumerateAudioEndPoints(DataFlow.Capture, DeviceState.Active);
+            for (int i = -1; i < NAudio.Wave.WaveIn.DeviceCount; i++)
+            {
+                var caps = NAudio.Wave.WaveIn.GetCapabilities(i);
+                Console.WriteLine($"{i}: {caps.ProductName}");
+            }
+        }
+
+        WaveFileWriter writer = null;
+        bool closing = false;
+        WaveInEvent waveIn;
+        bool _recording = false;
+
+        private void butTalk_Click(object sender, EventArgs e)
+        {
+            if (_recording)
+            {
+                waveIn.StopRecording();
+                this.UserMessage("Done recording");
+            }
+            else
+            {
+                _recording = true;
+                var outputFolder = Path.Combine(@"c:\temp", "faiRealTimeConversation");
+                Directory.CreateDirectory(outputFolder);
+                var outputFilePath = Path.Combine(outputFolder, "recorded.wav");
+                if(waveIn == null)
+                {
+                    //var cap = NAudio.Wave.WaveIn.GetCapabilities(1);
+                    //waveIn = new WaveInEvent();
+
+                    waveIn = new NAudio.Wave.WaveInEvent
+                    {
+                        DeviceNumber = -1,
+                        WaveFormat = new NAudio.Wave.WaveFormat(rate: 44100, bits: 16, channels: 1),
+                        BufferMilliseconds = 40
+                    };
+                }
+                waveIn.DataAvailable += (s, a) =>
+                {
+                    writer.Write(a.Buffer, 0, a.BytesRecorded);
+                    if (writer.Position > waveIn.WaveFormat.AverageBytesPerSecond * 16)
+                    {
+                        waveIn.StopRecording();
+                    }
+                };
+                waveIn.RecordingStopped += (s, a) =>
+                {
+                    writer?.Dispose();
+                    writer = null;
+                    if (closing)
+                    {
+                        waveIn.Dispose();
+                    }
+                };
+                writer = new WaveFileWriter(outputFilePath, waveIn.WaveFormat);
+                waveIn.StartRecording();
+                this.UserMessage("Recording");
+            }
+        }
+
+
+        private void UserMessage(string m)
+        {
+            this.txtUserOutput.Text += $"{m}{(m.EndsWith(Environment.NewLine) ? "" : Environment.NewLine)}";
+            this.txtUserOutput.SelectionStart = this.txtUserOutput.TextLength;
+            this.txtUserOutput.ScrollToCaret();
+            System.Windows.Forms.Application.DoEvents();
+        }
+    }
+}
