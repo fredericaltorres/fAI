c2ea19aca65963ad42147a5d1783e771cd81e966
FredericALTorres <frederic.torres@bigtincan.com>
2025-03-09 15:23:13 -0400

    Update...

--- src/fAI.Tests/TextSplitter/TextSplitter.tests.cs
+++ src/fAI.Tests/TextSplitter/TextSplitter.tests.cs
@@ -7,20 +7,9 @@ using System.IO;
 
 namespace fAI.Tests
 {
-    public class TextSplitterTestFixture : OpenAIUnitTestsBase, IDisposable 
-    {
-        public TextSplitterTestFixture()
-        {
-        }
-
-        public void Dispose()
-        {
-        }
-    }
-
     [Collection("Sequential")]
     [CollectionDefinition("Sequential", DisableParallelization = true)]
-    public class TextSplitter : IClassFixture<PineconeTestFixture>
+    public class TextSplitter : UnitTestBase
     {
         public TextSplitter()
         {
@@ -31,11 +20,11 @@ namespace fAI.Tests
         [TestBeforeAfter]
         public void SplitIntoParagraphs()
         {
-            var inputFileName = File.ReadAllText(@"C:\DVT\fAI\src\fAI.Tests\SpeechToText\LongText.txt");
-            var paragraphs = fAI.Util.TextSplitter.SplitIntoParagraphs(inputFileName, 128, 4096, 32);
+            var inputFileName = base.GetTestFile("LongText.txt");
+            var paragraphs = fAI.Util.TextSplitter.SplitIntoParagraphs(File.ReadAllText(inputFileName), 128, 4096, 32);
             ///paragraphs.ToFile(@"C:\DVT\fAI\src\fAI.Tests\SpeechToText\LongText.splitted.txt");
             Assert.Equal(55, paragraphs.Count);
-            var expectedParagraphs = new List<string>().FromFile(@"C:\DVT\fAI\src\fAI.Tests\SpeechToText\LongText.splitted.txt");
+            var expectedParagraphs = new List<string>().FromFile(base.GetTestFile("LongText.splitted.txt"));
 
             for (int i = 0; i < paragraphs.Count; i++)
                 Assert.Equal(expectedParagraphs[i], paragraphs[i]);
