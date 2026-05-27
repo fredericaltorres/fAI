9225dd37ce5df1c2af692fc184709cabaea35d7d
Frederic Torres <frederic.torres@showpad.com>
2026-05-27 16:38:36 -0400

    Update...

--- src/fAI.Lib/AIMemory/AIMemoryManager.cs
+++ src/fAI.Lib/AIMemory/AIMemoryManager.cs
@@ -600,15 +600,17 @@ namespace fAI
             var maxScore = aiMemories.Select(s => s.Score).Max();
             bm25MinimumScore = Math.Min(bm25MinimumScore, maxScore);
 
+            var maxToTrace = 20;
+
             // Order and trace the raw result
             aiMemories = new AIMemorys(aiMemories.OrderByDescending(d => d.Score).ToList());
-            TraceAIMemorys(new AIMemorys(aiMemories.ToList().Take(16).ToList()), $"BM25(0): query: {query}");
+            TraceAIMemorys(new AIMemorys(aiMemories.ToList().Take(maxToTrace).ToList()), $"BM25(0): query: {query}");
 
             Trace($"maxScore: {maxScore}, bm25MinimumScore: {bm25MinimumScore}");
 
             aiMemories = new AIMemorys(aiMemories.Where(d => d.Score >= bm25MinimumScore).OrderByDescending(d => d.Score).ToList());
             var minimumScoreOrModeStr = minimumScoreMode == -1 ? "Top 50%" : minimumScoreMode == -2 ? "Greater Than Std Deviation" : minimumScoreMode.ToString();
-            TraceAIMemorys(aiMemories, $"BM25(1): query: {query}");
+            TraceAIMemorys(new AIMemorys(aiMemories.ToList().Take(maxToTrace).ToList()), $"BM25(1): query: {query}");
 
             if (minimumScoreMode == -1)
             {
