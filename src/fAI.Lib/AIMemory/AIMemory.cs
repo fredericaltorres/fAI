1d5cd7cb68a4a305100d62312138c432f91d25be
Frederic Torres <frederic.torres@showpad.com>
2026-07-18 16:45:17 -0400

    Update...

--- src/fAI.Lib/AIMemory/AIMemory.cs
+++ src/fAI.Lib/AIMemory/AIMemory.cs
@@ -163,7 +163,7 @@ namespace fAI
 
         public override string ToString()
         {
-            return $"mid: {MID}, Score: {Score:0.000}, Title: {Title}, ModifiedDate: {ModifiedDate}, LocalFile: ({LocalFile}), Text: {Text}";
+            return $"mid: {MID}, Score: {Score:0.000}, Title: {Title}, ModifiedDate: {ModifiedDate}, LocalFile: ({LocalFile})"; // , Text: { Text}
         }
 
         public AIMemory Clone()
